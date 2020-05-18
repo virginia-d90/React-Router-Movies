6239a325a18dbed87a0b440a40b083cb943a359d
Virginia Davenport <virginialdport@gmail.com>
2020-05-18 14:17:35 -0600

    part one complete, first part of part 2 complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Route } from 'react-router-dom'
 
+//Components used for different routes
 import SavedList from './Movies/SavedList';
+
+//I added this not sure if I need it yet
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
+
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -28,7 +35,16 @@ const App = () => {
   return (
     <div>
       <SavedList list={savedList} />
-      <div>Replace this Div with your Routes</div>
+      {/* <div>Replace this Div with your Routes</div> */}
+      
+      <Route path='/movies/:movieID'>
+        <Movie movies={movieList} />
+      </Route>
+
+
+      <Route path='/'>
+        <MovieList key={movieList.id} movies={movieList} />
+      </Route>
     </div>
   );
 };
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link, useRouteMatch } from 'react-router-dom'
 
 const MovieList = props => {
+
+  
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
+          <MovieDetails key={movie.id} movie={movie} />
+      
+        
       ))}
     </div>
   );
@@ -12,23 +17,27 @@ const MovieList = props => {
 
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
 
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
+  const { url } = useRouteMatch()
+  return (
+    <Link to={`${url}/${movie.id}`}>
+      <div className="movie-card">
+        <h2>{title}</h2>
+        <div className="movie-director">
+          Director: <em>{director}</em>
         </div>
-      ))}
-    </div>
+        <div className="movie-metascore">
+          Metascore: <strong>{metascore}</strong>
+        </div>
+        <h3>Actors</h3>
+
+        {stars.map(star => (
+          <div key={star} className="movie-star">
+            {star}
+          </div>
+        ))}
+      </div>
+    </Link>
   );
 }
 
